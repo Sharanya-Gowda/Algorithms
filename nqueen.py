372b55c49aab4e8da16215e618fd3f7d62c0fa33
Sharanya M S <sharanyamavinaguni@gmail.com>
2024-08-07 00:53:05 +0530

    Update nqueen.py

--- nqueen.py
+++ nqueen.py
@@ -8,17 +8,14 @@ def printSolution(board):
         print()
 
 def isSafe(board, row, col):
-    # Check if there is a queen in the current row on the left side
     for i in range(col):
         if board[row][i] == 1:
             return False
 
-    # Check upper diagonal on the left side
     for i, j in zip(range(row, -1, -1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
 
-    # Check lower diagonal on the left side
     for i, j in zip(range(row, N, 1), range(col, -1, -1)):
         if board[i][j] == 1:
             return False
@@ -26,30 +23,18 @@ def isSafe(board, row, col):
     return True
 
 def solveNQUtil(board, col):
-    # If all queens are placed successfully
     if col >= N:
         printSolution(board)
-        print()  # Blank line between solutions
+        print()
         return
-
-    # Try placing this queen in all rows one by one
+        
     for i in range(N):
         if isSafe(board, i, col):
-            board[i][col] = 1  # Place queen
-
-            # Recur to place rest of the queens
+            board[i][col] = 1 
             solveNQUtil(board, col + 1)
-
-            # Backtrack
             board[i][col] = 0
 
 def solveNQ():
     board = [[0] * N for _ in range(N)]
-
     solveNQUtil(board, 0)
-
-    #if not any(1 in row for row in board):
-    #    print("No solutions exist")
-
-
 solveNQ()
